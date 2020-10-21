e43c6ba66518360fbf81762a1c10caf905017be7
Stefan Zivkovic <zivkovicstefan123@gmail.com>
2020-10-22 00:06:38 +0200

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axe from "@axe-core/react";
-import { MyCustomElement } from "./MyElem";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { MenuItem, Select, InputLabel } from "@material-ui/core";
+
+const BypassRepeatedBlocks = () => (
+  <div aria-hidden="true">
+    <a href="#select-content" style={{ display: "none" }}>
+      bypass repeated blocks
+    </a>
+  </div>
+);
+const NavigationSample = () => (
+  // we must have <nav> here in order to put content inside landmark. Otherwise we would get issue.
+  <nav>
+    <ul>
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+const HomePage = () => <div></div>;
+
 const App = () => (
-  <main>
-    <div>
-      <h1>
-        Just to overcome ARIA rule for having level-one heading, when on page
-        loading
-      </h1>
-    </div>
-    <h2>Expecting errors on opening the select HTML element</h2>
-    <MyCustomElement />
-  </main>
+  // BrowserRouter, Route and <Link> (NavigationSample) are the cause of bypass repeated blocks issue
+  <BrowserRouter>
+    {/**but with BypassRepeatedBlocks portion of code, it can be solved  */}
+    <BypassRepeatedBlocks />
+    <NavigationSample />
+
+    <main>
+      <Route path="/home" component={HomePage} />
+      {/**without <h1> there is heading issue on page loading */}
+      <h1>H1</h1>
+
+      <InputLabel id="my-input" style={{ color: "black" }}>
+        Sort
+      </InputLabel>
+      <Select
+        id="select-content"
+        labelId="my-input"
+        value="item1"
+        style={{ width: "200px" }}
+      >
+        <MenuItem key="item1" value="item1">
+          item1
+        </MenuItem>
+        <MenuItem key="item2" value="item2">
+          item2
+        </MenuItem>
+      </Select>
+    </main>
+  </BrowserRouter>
 );
 axe(React, ReactDOM, 1000);
 
